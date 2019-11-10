d7a180b0d9fdd9ddc56dffb8aa4427bcf2c798ea
marcosantonioreyesmedina <marcosantonioreyesmedina@gmail.com>
2019-11-10 16:11:27 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,124 @@
+// Configuracion previa para conectarse a Firebase
+
+
+const config = {
+    apiKey: 'AIzaSyDjvrHYhhV8Olh25XXCL4pkNb61i_J8orc',
+    authDomain: 'vuexample-2019.firebaseapp.com',
+    databaseURL: 'https://vuexample-2019.firebaseio.com',
+    projectId: 'vuexample-2019',
+    storageBucket: 'vuexample-2019.appspot.com',
+    messagingSenderId: '1095716700680',
+    appId: '1:1095716700680:web:b8cd3a3f6b804d0610c7bd'
+};
+
+// Inicializando la App
+
+firebase.initializeApp(config);
+
+// Obtengo la referencia a la BD
+
+const database = firebase.database();
+
+Vue.use(Toasted, {
+    iconPack: 'fontawesome',
+    position: 'top-center',
+    duration: 3000
+});
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        editing: false,
+        showPass: false,
+        allowUpdate: false,
+        filter: '',
+        newUser: {},
+        userList: []
+    },
+    created: function () {
+        database.ref('/users')
+            .on('value', snapshot => this.loadUser(snapshot.val()))
+            .then(() => console.info(this.userList));
+    },
+    computed: {
+        filterUser() {
+            return this.userList.filter((value) => {
+                return value.name.toLowerCase().includes(this.filter.toLowerCase()) ||
+                    value.lastname.toLowerCase().includes(this.filter.toLowerCase()) ||
+                    value.email.toLowerCase().includes(this.filter.toLowerCase());
+            });
+        }
+    },
+    methods: {
+        loadUser(users) {
+            this.userList = [];
+            for (let key in users) {
+                this.userList.push({
+                    key,
+                    name: users[key].name,
+                    lastname: users[key].lastname,
+                    email: users[key].email,
+                    pass: users[key].pass
+                });
+            }
+        },
+        addOrUpdateUser() {
+            let userPath = this.getUser(this.newUser.email);
+            let usuario = this.newUser.name;
+            // Escribo en la base de datos
+            database.ref(`/users/${userPath}`)
+                .set({
+                    name: this.newUser.name,
+                    lastname: this.newUser.lastname,
+                    email: this.newUser.email,
+                    pass: this.newUser.pass
+                })
+                .then(() => {
+                    if (this.editing) {
+                        this.$toasted.success(`El usuario: '${ usuario }' se ha actualizado satisfactoriamente`, {
+                            icon: 'check'
+                        });
+                        this.reset(true);
+                    } else {
+                        this.$toasted.success(`El usuario: '${ usuario }' se ha insertado satisfactoriamente`, {
+                            icon: 'check'
+                        });
+                        this.reset(false);
+                    }
+                });
+
+        },
+        deleteUser(email) {
+            let user = this.getUser(email);
+            let message = `Está seguro de eliminar el usuario ${user}`;
+            if (confirm(message)) {
+                database.ref('users').child(user)
+                    .set(null)
+                    .then(() => {
+                        this.$toasted.success(`El usuario: '${ user }' se ha eliminado satisfactoriamente`, {
+                            icon: 'check'
+                        });
+                    });
+            }
+        },
+        editUser(user) {
+            this.newUser = user;
+            this.editing = true;
+        },
+        getUser(email) {
+            let user = email.split('@')[0];
+            let re = /\./gi;
+            user = user.replace(re, "");
+            return user;
+        },
+        reset(cleanAll) {
+            if (cleanAll) {
+                this.editing = false;
+                this.showPass = false;
+                this.allowUpdate = false;
+            }
+            this.newUser = {};
+        }
+    }
+
+});
